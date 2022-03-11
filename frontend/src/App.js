36267a8a485dd187c60d73207d7c03fbeb52f216
mohamed-tahar <mohamedtahar.dbili@gmail.com>
2022-03-11 16:19:10 +0100

    create-mern-ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>amazona</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+export default App;
