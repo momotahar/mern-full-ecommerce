0ce5d606a9b7aaac61feddeac0144a5200f40da0
mohamed-tahar <mohamedtahar.dbili@gmail.com>
2022-03-11 22:11:38 +0100

    using-useReducer-to-fetchData

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import data from '../data'
-const HomeScreen = () => {
-  return (
-      <div>
-           <h1>Featured Products</h1>
-          <div className='products'>
-            {data.products?.map((product) => (
-              <div className='product' key={product.slug}>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                <div className='product-info'>
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            ))}
-          </div>
-    </div>
-  )
-}
+// import React, { useState } from "react";
+import { useEffect, useReducer } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import logger from 'use-reducer-logger'
+//Create reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, products: action.payload };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
-export default HomeScreen
+const HomeScreen = () => {
+  // const [products, setProducts] = useState([]);
+
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products:[],
+    loading: true,
+    error: "",
+  });
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: error.message });
+      }
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className='products'>
+        {loading ? <div>loading...</div> : error ? <div>{error}</div> :
+        (
+        products?.map((product) => (
+          <div className='product' key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className='product-info'>
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        )))}
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
